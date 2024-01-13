0c27091cfc974a85321c3cb095eb8601b8d3a6eb
bartha26 <155668307+bartha26@users.noreply.github.com>
2024-01-13 23:08:54 +0200

    Delete src/main directory

--- src/main/java/SimpleClasses/Shirt.java
+++
@@ -1,7 +0,0 @@
-package SimpleClasses;
-
-public class Shirt extends Products{
-    public Shirt(Integer productId, Double price, Integer companyId, String productName, String companyName) {
-        super(productId, price, companyId, productName, companyName);
-    }
-}
--- src/main/java/SimpleClasses/Shoes.java
+++
@@ -1,7 +0,0 @@
-package SimpleClasses;
-
-public class Shoes extends Products{
-    public Shoes(Integer productId, Double price, Integer companyId, String productName, String companyName) {
-        super(productId, price, companyId, productName, companyName);
-    }
-}
